01896af7827071af8271c71bab7c74f56e3fc342
nornadrina <nornadrina@gmail.com>
2022-02-21 16:53:55 -0300

    loop

--- for.js
+++ for.js
@@ -1,6 +1,12 @@
-function palidromcheck(input){
 
-	return input.split('').for(ir; condição-saída; expressão-final)
+var str = process.argv[2]; 
+let isPalindrom = true;
 
+for(i=0; i< str.length/2; i++){
+	 
+	if (str[i] != str[str.length -i -1]) {
+		 isPalindrom = false 
+	}
+ }
+console.log(isPalindrom)
 
-}
